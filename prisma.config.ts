42e8021cd9e4ce49f2366c68df72324ed89bd7b7
Mickael Aubin <mickaelaubin88@gmail.com>
2026-03-14 20:02:22 +0100

    prisma7 migration fixes

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,16 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import dotenv from "dotenv";
-import { defineConfig } from "prisma/config";
-
-dotenv.config({
-  quiet: true
-});
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL ?? ""
-  }
+    url: env("DATABASE_URL"),
+  },
 });
